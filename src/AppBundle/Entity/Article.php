33e902ea0de57a0c0e0bf1d8f946a656af7ca9b9
Delphine IDIART <idiart.delphine@yahoo.fr>
2015-04-05 16:49:19 +0200

    Maj Article

--- src/AppBundle/Entity/Article.php
+++ src/AppBundle/Entity/Article.php
@@ -1,14 +1,14 @@
 <?php
-
 namespace AppBundle\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
+use Doctrine\Common\Collections\ArrayCollection;
 
 /**
  * Article
  *
+ * @ORM\Table()
  * @ORM\Entity(repositoryClass="AppBundle\Entity\ArticleRepository")
- * @ORM\Table(name="article")
  */
 class Article
 {
@@ -20,39 +20,58 @@ class Article
      * @ORM\GeneratedValue(strategy="AUTO")
      */
     private $id;
-
     /**
      * @var string
      *
      * @ORM\Column(name="title", type="string", length=255)
      */
     private $title;
-
     /**
      * @var string
      *
      * @ORM\Column(name="content", type="string", length=255)
      */
     private $content;
-
     /**
      * @var \DateTime
      *
      * @ORM\Column(name="date", type="date")
      */
     private $date;
-
-
+    /**
+     * @var ArrayCollection
+     *
+     * @ORM\ManyToMany(targetEntity="Category")
+     */
+    private $categories;
+    /**
+     * @var ArrayCollection
+     *
+     * @ORM\ManyToMany(targetEntity="Tag")
+     */
+    private $tags;
+    /**
+     * @var User
+     *
+     * @ORM\ManyToOne(targetEntity="User")
+     */
+    private $user;
+    /**
+     * Constructor.
+     */
+    public function __construct()
+    {
+        $this->types = new ArrayCollection();
+    }
     /**
      * Get id
      *
-     * @return integer 
+     * @return integer
      */
     public function getId()
     {
         return $this->id;
     }
-
     /**
      * Set title
      *
@@ -62,20 +81,17 @@ class Article
     public function setTitle($title)
     {
         $this->title = $title;
-
         return $this;
     }
-
     /**
      * Get title
      *
-     * @return string 
+     * @return string
      */
     public function getTitle()
     {
         return $this->title;
     }
-
     /**
      * Set content
      *
@@ -85,20 +101,17 @@ class Article
     public function setContent($content)
     {
         $this->content = $content;
-
         return $this;
     }
-
     /**
      * Get content
      *
-     * @return string 
+     * @return string
      */
     public function getContent()
     {
         return $this->content;
     }
-
     /**
      * Set date
      *
@@ -108,20 +121,98 @@ class Article
     public function setDate($date)
     {
         $this->date = $date;
-
         return $this;
     }
-
     /**
      * Get date
      *
-     * @return \DateTime 
+     * @return \DateTime
      */
     public function getDate()
     {
         return $this->date;
     }
-
+    /**
+     * Add categories.
+     *
+     * @param Category $categories
+     *
+     * @return Article
+     */
+    public function addCategory(Category $categories)
+    {
+        $this->categories[] = $categories;
+        return $this;
+    }
+    /**
+     * Remove categories.
+     *
+     * @param Category $categories
+     */
+    public function removeCategory(Category $categories)
+    {
+        $this->types->removeElement($categories);
+    }
+    /**
+     * Get categories.
+     *
+     * @return \Doctrine\Common\Collections\Collection
+     */
+    public function getCategories()
+    {
+        return $this->categories;
+    }
+    /**
+     * Add tags.
+     *
+     * @param Tag $tags
+     *
+     * @return Article
+     */
+    public function addTag(Tag $tags)
+    {
+        $this->tags[] = $tags;
+        return $this;
+    }
+    /**
+     * Remove tags.
+     *
+     * @param Tag $tags
+     */
+    public function removeTag(Tag $tags)
+    {
+        $this->types->removeElement($tags);
+    }
+    /**
+     * Get tags.
+     *
+     * @return \Doctrine\Common\Collections\Collection
+     */
+    public function getTags()
+    {
+        return $this->tags;
+    }
+    /**
+     * Set user.
+     *
+     * @param User $user
+     *
+     * @return Article
+     */
+    public function setUser(User $user = null)
+    {
+        $this->user = $user;
+        return $this;
+    }
+    /**
+     * Get user.
+     *
+     * @return User
+     */
+    public function getUser()
+    {
+        return $this->user;
+    }
     /**
      * @return string
      */
@@ -129,5 +220,4 @@ class Article
     {
         return $this->title;
     }
-
 }
